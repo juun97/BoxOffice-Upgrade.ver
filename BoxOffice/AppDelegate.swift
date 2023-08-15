79e13ed8b5163e34916c4fb0cf4e2fc176d41674
Jun <59365211+juun97@users.noreply.github.com>
2023-08-15 23:53:21 +0900

    feat: 기존코드 업데이트

---
+++ BoxOffice/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  BoxOffice
+//
+//  Created by Rhode, Rilla on 2023/03/20.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+}
---
+++ BoxOffice/SceneDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  SceneDelegate.swift
+//  BoxOffice
+//
+//  Created by Rhode, Rilla on 2023/03/20.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        let rootViewController = BoxOfficeListViewController()
+        let navigationController = UINavigationController(rootViewController: rootViewController)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+}
