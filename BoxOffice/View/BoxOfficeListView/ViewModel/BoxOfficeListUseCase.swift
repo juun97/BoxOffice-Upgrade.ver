22e37506b34b52fdb0560976ea2f946be54c5835
Jun <59365211+juun97@users.noreply.github.com>
2023-08-16 20:35:16 +0900

    feat: RxSwift pod 설치

---
+++ BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  BoxOfficeListUseCase.swift
+//  BoxOffice
+//
+//  Created by 김성준 on 2023/08/16.
+//
+
+import Foundation
---
+++ BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  BoxOfficeListViewModel.swift
+//  BoxOffice
+//
+//  Created by 김성준 on 2023/08/16.
+//
+
+import Foundation
