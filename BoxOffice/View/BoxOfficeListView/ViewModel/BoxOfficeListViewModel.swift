8d1e77425b1a2a473066e37826a4005c4e853c48
Jun <59365211+juun97@users.noreply.github.com>
2023-08-18 02:36:28 +0900

    feat: BoxOfficeListViewModel 로직 추가

--- BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListViewModel.swift
+++ BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListViewModel.swift
@@ -10,8 +10,20 @@ import Foundation
 final class BoxOfficeListViewModel {
     
     private let useCase: BoxOfficeListUseCaseType
+    private(set) var boxOffice: BoxOffice?
+    private var currentDate: String = Date.yesterday.convertString(isFormatted: false)
     
     init(useCase: BoxOfficeListUseCaseType = BoxOfficeListUseCase()) {
         self.useCase = useCase
+    }
+    
+    func fetchData() {
+        useCase.fetchBoxOfficeData(dateString: currentDate)
+            .subscribe(onSuccess: { boxOffice in
+                self.boxOffice = boxOffice
+            }, onFailure: { error in
+                
+            })
+            .dispose()
     }
 }
