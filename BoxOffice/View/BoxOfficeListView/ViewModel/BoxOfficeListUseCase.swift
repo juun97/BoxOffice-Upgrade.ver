c777e3422f4deb3078226568ab8afa72fd00caf7
Jun <59365211+juun97@users.noreply.github.com>
2023-08-18 02:36:09 +0900

    feat : BoxOfficeListUseCase 비즈니스 로직 추가

--- BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListUseCase.swift
+++ BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListUseCase.swift
@@ -9,12 +9,42 @@ import Foundation
 import RxSwift
 
 protocol BoxOfficeListUseCaseType {
-    func fetchBoxOfficeData()
+    func fetchBoxOfficeData(dateString: String) -> Single<BoxOffice>
 }
 
 final class BoxOfficeListUseCase: BoxOfficeListUseCaseType {
-    func fetchBoxOfficeData() {
-        <#code#>
+    
+    private let networkManager = NetworkManager.shared
+    private let decoder = DecodeManager()
+    private let urlMaker = URLRequestMaker()
+    private var currentDate: String = Date.yesterday.convertString(isFormatted: false)
+    
+    func fetchBoxOfficeData(dateString: String) -> Single<BoxOffice>  {
+        return Single.create { observer in
+            guard let request = self.urlMaker.makeBoxOfficeURLRequest(date: dateString) else {
+                return Disposables.create()
+            }
+            
+            self.networkManager.fetchData(request: request)
+                .subscribe(onSuccess: { data in
+                    let decodedData = DecodeManager().decodeJSON(data: data, type: BoxOffice.self)
+                    
+                    switch decodedData {
+                    case .success(let boxOffice):
+                        observer(.success(boxOffice))
+                    case .failure(let error):
+                        observer(.failure(error))
+                    }
+                    
+                }, onFailure: { error in
+                    observer(.failure(error))
+                })
+                .dispose()
+            
+            return Disposables.create()
+        }
+
+        
     }
     
     
