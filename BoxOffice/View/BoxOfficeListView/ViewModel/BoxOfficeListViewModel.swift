91918a694a73ae303f071aead5c2e2eb11e4cdd1
Jun <59365211+juun97@users.noreply.github.com>
2023-08-23 02:53:47 +0900

    feat : 관찰가능한 boxOffice 모델 프로퍼티 구현

--- BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListViewModel.swift
+++ BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListViewModel.swift
@@ -6,12 +6,24 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 final class BoxOfficeListViewModel {
     
     private let useCase: BoxOfficeListUseCaseType
     private(set) var boxOffice: BoxOffice?
     private var currentDate: String = Date.yesterday.convertString(isFormatted: false)
+    
+    private let _boxOffice2 = BehaviorRelay<BoxOffice?>(value: nil)
+    
+    var boxOffice2: Observable<BoxOffice?> {
+        return _boxOffice2.asObservable()
+    }
+    
+    private func updateBoxOffice(newValue: BoxOffice) {
+        _boxOffice2.accept(newValue)
+    }
     
     init(useCase: BoxOfficeListUseCaseType = BoxOfficeListUseCase()) {
         self.useCase = useCase
