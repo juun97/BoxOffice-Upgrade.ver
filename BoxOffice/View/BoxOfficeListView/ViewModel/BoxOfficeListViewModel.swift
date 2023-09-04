1c38cd2ad80fe58784f11d3a227672a6ea085880
Jun <59365211+juun97@users.noreply.github.com>
2023-09-04 20:44:50 +0900

    refactor: ViewModel Observable 구독 관리

--- BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListViewModel.swift
+++ BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListViewModel.swift
@@ -12,31 +12,31 @@ import RxCocoa
 final class BoxOfficeListViewModel {
     
     private let useCase: BoxOfficeListUseCaseType
-    private(set) var boxOffice: BoxOffice?
     private var currentDate: String = Date.yesterday.convertString(isFormatted: false)
+    let disposeBag = DisposeBag()
     
-    let _boxOffice2 = BehaviorRelay<[MainDataSection]>(value: [])
+    private let _boxOffice = BehaviorRelay<[MainDataSection]>(value: [])
     
-    var boxOffice2: Observable<[MainDataSection]> {
-        return _boxOffice2.asObservable()
-    }
-    
-    private func updateBoxOffice(newValue: [DailyBoxOffice]) {
-        let dataSection = newValue.map{ _ in MainDataSection(header: "main", items: newValue) }
-        _boxOffice2.accept(dataSection)
+    var boxOffice: Observable<[MainDataSection]> {
+        return _boxOffice.asObservable()
     }
     
     init(useCase: BoxOfficeListUseCaseType = BoxOfficeListUseCase()) {
         self.useCase = useCase
     }
     
+    private func updateBoxOffice(newValue: [DailyBoxOffice]) {
+        let dataSection =  MainDataSection(header: "main", items: newValue)
+        _boxOffice.accept([dataSection])
+    }
+    
     func fetchData() {
         useCase.fetchBoxOfficeData(dateString: currentDate)
-            .subscribe(onSuccess: { boxOffice in
-                self.updateBoxOffice(newValue: boxOffice.boxOfficeResult.dailyBoxOfficeList)
-            }, onFailure: { error in
-                
+            .subscribe(onNext: { boxOfficeList in
+                self.updateBoxOffice(newValue: boxOfficeList)
+            }, onError: { error in
+                print(error.localizedDescription)
             })
-            .dispose()
+            .disposed(by: disposeBag)
     }
 }
