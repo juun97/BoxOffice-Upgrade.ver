e4a80ae57e6039553165e4a226cd77b2cfe2e4b1
Jun <59365211+juun97@users.noreply.github.com>
2023-09-04 20:43:01 +0900

    refactor: fetchData 로직 산소화

--- BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListUseCase.swift
+++ BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListUseCase.swift
@@ -8,44 +8,27 @@
 import Foundation
 import RxSwift
 
+enum BoxOfficeListUseCaseError: Error {
+    case networkError(NetworkError)
+    case decodeError
+    case invalidRequest
+}
+
 protocol BoxOfficeListUseCaseType {
-    func fetchBoxOfficeData(dateString: String) -> Single<BoxOffice>
+    func fetchBoxOfficeData(dateString: String) -> Observable<[DailyBoxOffice]>
 }
 
 final class BoxOfficeListUseCase: BoxOfficeListUseCaseType {
-    
-    private let networkManager = NetworkManager.shared
-    private let decoder = DecodeManager()
     private let urlMaker = URLRequestMaker()
-    private var currentDate: String = Date.yesterday.convertString(isFormatted: false)
-    
-    func fetchBoxOfficeData(dateString: String) -> Single<BoxOffice>  {
-        return Single.create { observer in
-            guard let request = self.urlMaker.makeBoxOfficeURLRequest(date: dateString) else {
-                return Disposables.create()
-            }
-            
-            self.networkManager.fetchData(request: request)
-                .subscribe(onSuccess: { data in
-                    let decodedData = DecodeManager().decodeJSON(data: data, type: BoxOffice.self)
-                    
-                    switch decodedData {
-                    case .success(let boxOffice):
-                        observer(.success(boxOffice))
-                    case .failure(let error):
-                        observer(.failure(error))
-                    }
-                    
-                }, onFailure: { error in
-                    observer(.failure(error))
-                })
-                .dispose()
-            
-            return Disposables.create()
-        }
 
+    func fetchBoxOfficeData(dateString: String) -> Observable<[DailyBoxOffice]> {
+        guard let request = self.urlMaker.makeBoxOfficeURLRequest(date: dateString) else {
+            return .error(BoxOfficeListUseCaseError.invalidRequest)
+        }
         
+        return NetworkManager.shared.fetchData(request: request)
+            .decode(type: BoxOffice.self, decoder: JSONDecoder())
+            .map { $0.boxOfficeResult.dailyBoxOfficeList }
     }
-    
-    
 }
+
