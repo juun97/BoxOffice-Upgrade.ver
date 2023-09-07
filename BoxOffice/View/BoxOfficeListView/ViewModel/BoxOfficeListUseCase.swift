fcbe65a9943a633ff6d8b56f16e7d9396db8ec7b
Jun <59365211+juun97@users.noreply.github.com>
2023-09-07 11:54:13 +0900

    feat: UseCase에서 CellMode 읽기, 저장 구현

--- BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListUseCase.swift
+++ BoxOffice/View/BoxOfficeListView/ViewModel/BoxOfficeListUseCase.swift
@@ -16,11 +16,13 @@ enum BoxOfficeListUseCaseError: Error {
 
 protocol BoxOfficeListUseCaseType {
     func fetchBoxOfficeData(dateString: String) -> Observable<[DailyBoxOffice]>
+    func readCellMode() -> CellMode
+    func saveCellMode(_ cellMode: CellMode)
 }
 
 final class BoxOfficeListUseCase: BoxOfficeListUseCaseType {
     private let urlMaker = URLRequestMaker()
-
+    
     func fetchBoxOfficeData(dateString: String) -> Observable<[DailyBoxOffice]> {
         guard let request = self.urlMaker.makeBoxOfficeURLRequest(date: dateString) else {
             return .error(BoxOfficeListUseCaseError.invalidRequest)
@@ -30,5 +32,19 @@ final class BoxOfficeListUseCase: BoxOfficeListUseCaseType {
             .decode(type: BoxOffice.self, decoder: JSONDecoder())
             .map { $0.boxOfficeResult.dailyBoxOfficeList }
     }
+    
+    func readCellMode() -> CellMode {
+        guard let data = UserDefaults.standard.data(forKey: CellMode.identifier),
+              let cellMode = try? DecodeManager().decodeJSON(data: data, type: CellMode.self) else {
+            return .list
+        }
+        
+        return cellMode
+    }
+    
+    func saveCellMode(_ cellMode: CellMode) {
+        let encoder = JSONEncoder()
+        let encodedData = try? encoder.encode(cellMode)
+        UserDefaults.standard.set(encodedData, forKey: CellMode.identifier)
+    }
 }
-
