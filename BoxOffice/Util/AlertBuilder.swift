9fa2ca093aa7180d7cde15dff8f93c72e332e526
Jun <59365211+juun97@users.noreply.github.com>
2023-09-07 18:04:06 +0900

    feat: AlertBuilder 타입 생성 및 구현

--- BoxOffice/Util/AlertBuilder.swift
+++ BoxOffice/Util/AlertBuilder.swift
@@ -5,4 +5,48 @@
 //  Created by 김성준 on 2023/09/07.
 //
 
-import Foundation
+import UIKit
+
+final class AlertBuilder {
+    private var preferredStyle: UIAlertController.Style = .alert
+    private var title: String? = nil
+    private var message: String? = nil
+    private var actions: [UIAlertAction] = [UIAlertAction]()
+
+    
+    func preferredStyle(_ style: UIAlertController.Style) -> AlertBuilder {
+        self.preferredStyle = style
+        return self
+    }
+    
+    func withTitle(_ title: String?) -> AlertBuilder {
+        self.title = title
+        return self
+    }
+    
+    func withMessage(_ message: String?) -> AlertBuilder {
+        self.message = message
+        return self
+    }
+    
+    func addAction(_ title: String, style: UIAlertAction.Style, handler: ((UIAlertAction) -> Void)? = nil) -> AlertBuilder {
+        let action = UIAlertAction(title: title, style: style, handler: handler)
+        actions.append(action)
+        return self
+    }
+    
+    func show(in viewController: UIViewController, animated: Bool = true) {
+        viewController.present(build(), animated: animated)
+    }
+    
+    private func build() -> UIAlertController {
+        let alert = UIAlertController(title: self.title, message: self.message, preferredStyle: self.preferredStyle)
+        
+  
+        actions.forEach { action in
+            alert.addAction(action)
+        }
+        
+        return alert
+    }
+}
