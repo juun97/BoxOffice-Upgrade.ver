46899cbb367d68c9dc918d824c45012a03ad4cca
Jun <59365211+juun97@users.noreply.github.com>
2023-09-14 20:14:14 +0900

    feat: DetailMovieUseCase 기능구현

--- BoxOffice/View/DetailMovieView/ViewModel/DetailMovieUseCase.swift
+++ BoxOffice/View/DetailMovieView/ViewModel/DetailMovieUseCase.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import RxSwift
+
+enum DetailMovieUseCaseError: Error {
+    case networkError(NetworkError)
+    case decodeError
+    case invalidRequest
+}
+
+protocol DetailMovieUseCaseType {
+    func fetchMovieInformation(movieCode: String) -> Observable<DetailMovieInformation>
+    func fetchMoviePoster(movieName: String) -> Observable<MoviePoster>
+}
+
+final class DetailMovieUseCase: DetailMovieUseCaseType {
+    
+    func fetchMovieInformation(movieCode: String) -> Observable<DetailMovieInformation> {
+        guard let request = URLRequestMaker().makeMovieInformationURLRequest(movieCode: movieCode) else {
+            return .error(DetailMovieUseCaseError.invalidRequest)
+        }
+        
+        return NetworkManager.shared.fetchData(request: request)
+            .decode(type: DetailMovieInformation.self, decoder: JSONDecoder())
+    }
+    
+    func fetchMoviePoster(movieName: String) -> Observable<MoviePoster> {
+        guard let request = URLRequestMaker().makeMoviePosterURLRequest(movieName: movieName) else {
+            return .error(DetailMovieUseCaseError.invalidRequest)
+        }
+        
+        return NetworkManager.shared.fetchData(request: request)
+            .decode(type: MoviePoster.self, decoder: JSONDecoder())
+        
+    }
+}
