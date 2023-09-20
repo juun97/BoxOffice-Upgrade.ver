8f10019f1b86a4a8b3be1287f76532d5f3e07858
Jun <59365211+juun97@users.noreply.github.com>
2023-09-20 19:44:35 +0900

    feat: Notification.Name extension 구현

---
+++ BoxOffice/Util/Extension/Notification.Name+.swift
@@ -0,0 +1,12 @@
+//
+//  Notification.Name+.swift
+//  BoxOffice
+//
+//  Created by 김성준 on 2023/09/19.
+//
+
+import Foundation
+
+extension Notification.Name {
+    static let calendarViewDismiss = Notification.Name(rawValue: "calendarViewDismiss")
+}
